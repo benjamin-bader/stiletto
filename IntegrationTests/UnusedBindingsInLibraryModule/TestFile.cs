98f719777d2e766822b1e20f027aea5471a9b750
Ben Bader <ben.bader@rd.io>
2013-06-16 21:51:38 -0700

    Adding a test verifying IsLibrary suppression of unused binding errors.

---
+++ IntegrationTests/UnusedBindingsInLibraryModule/TestFile.cs
@@ -0,0 +1,27 @@
+﻿using Stiletto;
+
+namespace UnusedBindingsInLibraryModule
+{
+    public class InjectableClass
+    {
+        [Inject]
+        public string Foo { get; set; }
+    }
+
+    [Module(EntryPoints = new[] { typeof(InjectableClass) },
+            IsLibrary = true)]
+    public class MainModule
+    {
+        [Provides]
+        public string ProvideString()
+        {
+            return "foo";
+        }
+
+        [Provides]
+        public object ProvideObject()
+        {
+            return new object();
+        }
+    }
+}
