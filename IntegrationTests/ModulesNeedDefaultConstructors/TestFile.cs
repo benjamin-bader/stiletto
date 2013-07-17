9b611c1aec0ea0cbd21dc8b98b424ad79cbba34c
Ben Bader <ben.bader@rd.io>
2013-07-17 15:12:21 -0700

    Adding a unit test for weaver validation of module constructors.

---
+++ IntegrationTests/ModulesNeedDefaultConstructors/TestFile.cs
@@ -0,0 +1,34 @@
+﻿using Stiletto;
+
+namespace ModulesNeedDefaultConstructors
+{
+    public class Foo
+    {
+        [Inject]
+        public Bar Bar { get; set; }
+    }
+
+    public class Bar
+    {
+        [Inject]
+        public Foo Foo { get; set; }
+    }
+
+    public class Foobar
+    {
+        [Inject]
+        public Foobar(Foo foo, Bar bar)
+        {
+            
+        }
+    }
+
+    [Module(Injects = new[] { typeof(Foobar) })]
+    public class MainModule
+    {
+        public MainModule(int someParam)
+        {
+
+        }
+    }
+}
