8024b989b87516e9217f37f52505330e1da9cfb5
Benjamin Bader <ben@getkeepsafe.com>
2013-11-15 12:16:46 -0800

    Adding tests around enforcing/suppressing orphan analysis of sets

---
+++ IntegrationTests/OrphanNonLibrarySetsFail/TestFile.cs
@@ -0,0 +1,41 @@
+﻿using Stiletto;
+
+namespace OrphanNonLibrarySetsFail
+{
+    public class InjectableClass
+    {
+        [Inject]
+        public object Obj { get; set; }
+    }
+
+    [Module(Injects = new[] { typeof(InjectableClass) },
+        IncludedModules = new[] { typeof(SetLibraryModule), typeof(SetNonLibraryModule) })]
+    public class MainSetLibraryModule
+    {
+        [Provides]
+        public object ProvideObject()
+        {
+            return new object();
+        }
+    }
+
+    [Module(IsLibrary = true)]
+    public class SetLibraryModule
+    {
+        [Provides(ProvidesType.Set)]
+        public string ProvideFoo()
+        {
+            return "";
+        }
+    }
+
+    [Module]
+    public class SetNonLibraryModule
+    {
+        [Provides(ProvidesType.Set)]
+        public string ProvideBar()
+        {
+            return "bar";
+        }
+    }
+}
